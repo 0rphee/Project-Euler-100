6f87cf6c4a0497a1f0cc93b9779ca98a05d673f1
0rphee <79347623+0rphee@users.noreply.github.com>
2022-10-31 22:38:35 -0600

    largest-palindrome-product solved

--- 04-Largest-palindrome-product/largest-palindrome-product.hs
+++ 04-Largest-palindrome-product/largest-palindrome-product.hs
@@ -1,36 +1,37 @@
+{-# LANGUAGE TupleSections #-}
 
-main :: IO ()
-main = print $ foldl fun Nothing final 
-
-isPalindrome :: Int -> Int -> Bool
-isPalindrome x y = standard == reversed
-  where standard = show $ x * y
+isPalindrome :: Int -> Bool
+isPalindrome num = standard == reversed
+  where standard = show num
         reversed = reverse standard
+-- foldl :: (b -> a -> b) -> t a -> b 
+data Result = Result Int (Int, Int)
+  deriving (Show, Eq)
+
+foldingFunc :: Maybe Result -> (Int, Int) -> Maybe Result
+foldingFunc (Just (Result a (j, k))) (x, y)
+  | isPalindrome num = if num > a
+                       then Just (Result num (x, y))
+                       else Just (Result a (j, k))
+  | otherwise = Just (Result a (j, k))
+  where num = x * y
+foldingFunc Nothing (x, y)
+  | isPalindrome num = Just $ Result num (x, y)
+  | otherwise = Nothing
+  where num = x * y
 
 availableNums :: [Int]
 availableNums = [100..999]
 
--- :: (b -> a -> b) -> t a -> b 
-data Result = Result Int (Int, Int)
-  deriving (Show, Eq)
+combine :: [Int] -> [Int] -> [(Int, Int)]
+combine l1 l2 = concat ll
+  where first = fmap (,) l1
+        ll = fmap (\x -> fmap (,x) l1) l2
 
-fun :: Maybe Result -> (Int, Int) -> Maybe Result
-fun (Just (Result a (j, k))) (x, y)
-  | isPalindrome x y = if (x*y) > a
-                       then Just (Result (x*y) (x, y))
-                       else Just (Result a (j, k))
-  | otherwise = Just (Result a (j, k))
-fun Nothing (x, y)
-  | isPalindrome x y = Just $ Result (x*y) (x, y)
-  | otherwise = Nothing
+availableCombinations :: [(Int, Int)]
+availableCombinations = combine availableNums availableNums
 
-orderedList :: [(Int, Int)]
-orderedList = zip availableNums availableNums
+main :: IO ()
+main = print $ foldl foldingFunc Nothing availableCombinations
 
-ordRev = zip availableNums (reverse availableNums)
-
-final = orderedList ++ ordRev
-
-
-
-
+-- Just (Result 906609 (993,913))
