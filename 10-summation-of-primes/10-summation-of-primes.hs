fee488c4ac399950fba5172e99c9d3c9870e2cb0
0rphee <79347623+0rphee@users.noreply.github.com>
2022-11-05 18:15:33 -0600

    change prime sieve

--- 10-summation-of-primes/10-summation-of-primes.hs
+++ 10-summation-of-primes/10-summation-of-primes.hs
@@ -1,33 +1,13 @@
+eratosSieve :: Int -> [Int]
+eratosSieve num = h [2..num] 0
+  where h :: [Int] -> Int -> [Int]
+        h [] _ = []
+        h ll nDrop
+          | y^2 <= num = h (prev ++ x:y:ys ) (nDrop+1)
+          | otherwise = prev ++ x:y:ys
+          where (x:xs) = drop nDrop ll
+                prev = take nDrop ll
+                (y:ys) = filter (\w->w`mod`x/=0) xs
 
-getNPrimes :: Int -> [Int]
-getNPrimes numOfPrimesToGet = helper 0 2 [2]
-  where helper :: Int -> Int -> [Int] -> [Int]
-        helper count carry primeList
-          | count < numOfPrimesToGet = helper (count+1) (head nextList) nextList
-          | otherwise = nextList
-          where nextList = nextPrime primeList carry
- 
-
-getPrimesUntil :: Int -> [Int]
-getPrimesUntil maxPrime = helper 2 [2]
-  where helper :: Int -> [Int] -> [Int]
-        helper carry primeList
-          | nextCarry < maxPrime = helper nextCarry nextList
-          | otherwise = tail nextList
-          where nextList = nextPrime primeList carry
-                nextCarry = head nextList
-
-
-nextPrime :: [Int] -> Int -> [Int]
-nextPrime [] _ = []
-nextPrime primeList carry
-  | any numIsFactor primeList = nextPrime primeList nextNum
-  | otherwise = nextNum:primeList
-  where nextNum = carry + 1
-        numIsFactor y = nextNum `mod` y == 0
-        
-newNextP num = [ a | a <- [1..num], num `mod` a == 0 ]
-
-        
-main = print $ sum $ getPrimesUntil 2000000
+main = print $ sum $ eratosSieve 2000000
 
