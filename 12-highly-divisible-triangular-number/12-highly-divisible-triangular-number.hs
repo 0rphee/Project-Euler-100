b30f777e20cc4bacf9c2e17a4f141aa08ed86040
0rphee <79347623+0rphee@users.noreply.github.com>
2022-11-05 23:21:47 -0600

    minor adjustments, already finished

--- 12-highly-divisible-triangular-number/12-highly-divisible-triangular-number.hs
+++ 12-highly-divisible-triangular-number/12-highly-divisible-triangular-number.hs
@@ -1,15 +1,12 @@
-import Data.Maybe (isJust, fromJust)
 
-isFactor :: Int -> Int -> Maybe Int
-isFactor num x = if modulus == 0
-                 then Just dividend
-                 else Nothing
-  where (dividend, modulus)= num `divMod` x
+isFactor :: Int -> Int -> (Int, Bool)
+isFactor num x = (dividend, modulus == 0)
+  where (dividend, modulus) = num `divMod` x
 
 getFactors :: Int -> [Int]
-getFactors num = concat [ a:[fromJust may] | a <- [1.. round (sqrt $ fromIntegral num)]
-                        , let may = isFactor num a
-                        , isJust may
+getFactors num = concat [ a:[dividend] | a <- [1.. round (sqrt $ fromIntegral num)]
+                        , let (dividend, factor)= isFactor num a
+                        , factor
                         ]
 
 triangleNums :: [Int]
